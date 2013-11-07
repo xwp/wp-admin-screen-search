b637253a7113bbd6d55d825d31470f11b5fc2031
johnregan3 <john.regan@x-team.com>
2013-11-07 17:32:53 -0600

    Polshing First look at Progress Bar

--- admin-search.js
+++ admin-search.js
@@ -52,11 +52,11 @@
 
 	function buildProgressBar () {
 		$( '#wpadminbar' ).prepend( '<div id="admin-search-progress-bar"><div></div></div>' );
-		$( '#admin-search-progress-bar' ).css( { 'height': '3px', 'backgroundColor' : '#777' } );
+		$( '#admin-search-progress-bar' ).css( { 'height': '2px' } );
 	}
 
 	function indexAdminScreens( totalScreens ) {
-		var loadedScreens = 0;
+		var loadedScreens = 1;
 		$( '#adminmenu li > a' ).each( function() {
 			var label = '';
 			if ( $( this ).hasClass( 'wp-has-submenu' ) ) {
@@ -65,26 +65,31 @@
 				label = $( this ).text();
 			}
 			$.get( $( this ).attr( 'href' ), function( data ) {
-				sendScreenMarkup( label, this.url, data )
+				sendScreenMarkup( label, this.url, data, loadedScreens, totalScreens );
 				loadedScreens++
-				increaseProgressBar( loadedScreens, totalScreens);
 			});
 		});
 	}
 
-	function sendScreenMarkup( label, path, markup ) {
+	function sendScreenMarkup( label, path, markup, loadedScreens, totalScreens ) {
 		$.ajax({
 			type : 'post',
 			dataType : 'json',
 			url : screenIndexer.ajaxurl,
-			data : { action: 'update_search_index', label : label, path : path, markup : markup }
+			data : { action: 'update_search_index', label : label, path : path, markup : markup },
+			success : function() {
+				increaseProgressBar( loadedScreens, totalScreens );
+			}
 		});
 	}
 
 	function increaseProgressBar( loadedScreens, totalScreens ) {
 		var percentage = ( loadedScreens / totalScreens ) * 100;
-		console.log( percentage );
-		$( '#admin-search-progress-bar div' ).css( { 'height': '3px', 'backgroundColor' : '#0074a2', 'width' : percentage + '%' } );
+		console.log( loadedScreens + ' : ' + percentage );
+		$( '#admin-search-progress-bar div' ).css( {'height' : '2px', 'backgroundColor' : '#0074a2', 'width' : percentage + '%' } );
+		if ( percentage == 100 ) {
+			$( '#admin-search-progress-bar div' ).delay( 1000 ).fadeOut( 'slow' );
+		}
 	}
 
 	function menuResults( data ) {
